97ab75e5792c5918b72e46d6234969f39f3d571a
tetsuyainfra <tetsuyainfra@gmail.com>
2025-04-19 23:01:59 +0900

    SITE_NAME, SITE_DESC環境変数を追加

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -1,7 +1,7 @@
 export default function () {
   return {
-    title: "DevYP",
-    description: "開発向けYP",
+    title: process.env.SITE_NAME || "DevYP",
+    description: process.env.SITE_DESC || "開発向けYP",
     indexTxtUrl: process.env.INDEX_TXT_URL || "http://localhost/index.txt",
     indexJsonUrl:
       process.env.INDEX_JSON_URL || "http://localhost:7143/api/index.json",
