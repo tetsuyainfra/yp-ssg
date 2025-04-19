f12b9fb0691de597edd984f0687e64f67a49887a
tetsuyainfra <tetsuyainfra@gmail.com>
2025-04-19 22:19:18 +0900

    とりあえずindex.jsonを読み取れるようになった

--- src/client/index_show.tsx
+++ src/client/index_show.tsx
@@ -1,8 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import useSWR from "swr";
+
+const fetcher = (url: string) =>
+  fetch(url)
+    .then((res) => {
+      console.log(`url(${url})->res`, res);
+      return res.json();
+    })
+    .then((data) => {
+      console.log(`url(${url})->data`, data);
+      return data;
+    });
 
 function IndexComponent() {
-  return <div>IndexComponetnt</div>;
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:7143/api/index.json",
+    fetcher
+  );
+  return (
+    <div>
+      <h1>IndexComponent</h1>
+      <ul>
+        {data &&
+          data.map((c, i) => {
+            return <li key={i}>{c.name}</li>;
+          })}
+      </ul>
+    </div>
+  );
 }
 
 function renderIndexShow(id: string) {
